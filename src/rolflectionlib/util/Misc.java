b62559b554634c3fefcde5f57fe9fd37bdeb3fba
rolfosian <135147406+rolfosian@users.noreply.github.com>
2026-01-12 03:27:37 +1300

    Update Misc.java

--- src/rolflectionlib/util/Misc.java
+++ src/rolflectionlib/util/Misc.java
@@ -4,32 +4,12 @@ import java.util.*;
 import org.apache.log4j.Logger;
 
 import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.campaign.CampaignUIAPI;
-import com.fs.starfarer.api.campaign.InteractionDialogAPI;
 import com.fs.starfarer.api.campaign.SectorEntityToken;
-import com.fs.starfarer.api.input.InputEventClass;
-import com.fs.starfarer.api.input.InputEventType;
-import com.fs.starfarer.api.ui.ButtonAPI;
-import com.fs.starfarer.api.ui.LabelAPI;
-import com.fs.starfarer.api.ui.PositionAPI;
-import com.fs.starfarer.api.ui.UIPanelAPI;
 
 import com.fs.starfarer.api.loading.Description;
 
-import rolflectionlib.ui.UiUtil.DialogDismissedListener;
-
 @SuppressWarnings("unchecked")
 public class Misc {
-    private static final Logger logger = Global.getLogger(Misc.class);
-    public static void print(Object... args) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < args.length; i++) {
-            sb.append(args[i] instanceof String ? (String) args[i] : String.valueOf(args[i]));
-            if (i < args.length - 1) sb.append(' ');
-        }
-        logger.info(sb.toString());
-    }
-
     public static Map<String, Object> DESCRIPTION_MAP;
     public static final String CUSTOM_DESC_PERSISTENT_DATA_KEY = "$rlCustomCampaignEntityDescs";
     public static final List<String> CUSTOM_DESC_KEYS = new ArrayList<>();
@@ -45,93 +25,5 @@ public class Misc {
 
         CUSTOM_DESC_KEYS.add(key);
         entity.setCustomDescriptionId(entity.getId());
-    }
-
-    public static Object createButtonClickEventInstance(PositionAPI buttonPosition) {
-        return createInputEventInstance(
-        InputEventClass.MOUSE_EVENT,
-        InputEventType.MOUSE_DOWN,
-        (int)buttonPosition.getCenterX(),
-        (int)buttonPosition.getCenterY(),
-        0, // LMB
-        '\0' // unused?
-        );
-    }
-
-    public static Object createInputEventInstance(InputEventClass eventClass, InputEventType eventType, int x, int y, int val, char char_) {
-        return RolfLectionUtil.instantiateClass(
-            ClassRefs.inputEventClassConstructor,
-            eventClass,
-            eventType,
-            x,
-            y,
-            val, // keyboard key or mouse button, is -1 for mouse move
-            char_ // char is only appicable for keyboard keys afaik, give '\0' for mouse prob
-        );
-    }
-
-    public static void clickButton(ButtonAPI button) {
-        if (button == null) return;
-
-        Object listener = RolfLectionUtil.invokeMethodDirectly(ClassRefs.buttonGetListenerMethod, button);
-        RolfLectionUtil.invokeMethodDirectly(ClassRefs.buttonListenerActionPerformedMethod, listener, createButtonClickEventInstance(((ButtonAPI)button).getPosition()), button);
-    }
-
-    /**
-     * Only works in campaign.
-     * @return CoreUI
-     */
-    public static UIPanelAPI getCoreUI() {
-        CampaignUIAPI campaignUI = Global.getSector().getCampaignUI();
-        InteractionDialogAPI dialog = campaignUI.getCurrentInteractionDialog();
-
-        return dialog == null ? (UIPanelAPI) RolfLectionUtil.invokeMethodDirectly(ClassRefs.campaignUIGetCoreMethod, campaignUI) : (UIPanelAPI) RolfLectionUtil.invokeMethodDirectly(ClassRefs.interactionDialogGetCoreUIMethod, dialog);
-    }
-
-    public static Object createConfirmDialog(String text, String confirmText, String cancelText, float width, float height, DialogDismissedListener dialogListener, Object screenPanel) {
-        return RolfLectionUtil.instantiateClass(
-            ClassRefs.confirmDialogConstructor,
-            width,
-            height,
-            screenPanel,
-            dialogListener.getProxy(),
-            text,
-            new String[]{confirmText, cancelText}
-        );
-    }
-
-    /**
-     * 
-     * @param title Confirm dialog window title
-     * @param confirmText Confirm button text
-     * @param cancelText Cancel button text
-     * @param width Dialog window width
-     * @param height Dialog window height
-     * @param dialogListener Listener implementation that calls {@link DialogDismissedListener} trigger method when the confirm dialog is closed
-     * @param screenPanel Panel field the confirm dialog anchors to. In most cases is the screenPanel field of CampaignUI. 
-     * @return Object array containing dialog components in this order: [0] Title label, [1] Confirm button, [2] Cancel button, [3] Confirm dialog object itself
-     */
-    public static Object[] showConfirmationDialog(
-        String title,
-        String confirmText,
-        String cancelText,
-        float width,
-        float height,
-        DialogDismissedListener dialogListener,
-        Object screenPanel
-    ) {
-
-        Object confirmDialog = createConfirmDialog(title, confirmText, cancelText, width, height, dialogListener, screenPanel);
-        RolfLectionUtil.invokeMethodDirectly(ClassRefs.confirmDialogShowMethod, confirmDialog, 0.25f, 0.25f);
-
-        LabelAPI label = (LabelAPI) RolfLectionUtil.invokeMethodDirectly(ClassRefs.confirmDialogGetLabelMethod, confirmDialog);
-        ButtonAPI yes = (ButtonAPI) RolfLectionUtil.invokeMethodDirectly(ClassRefs.confirmDialogGetButtonMethod, confirmDialog, 0);
-        ButtonAPI no = (ButtonAPI) RolfLectionUtil.invokeMethodDirectly(ClassRefs.confirmDialogGetButtonMethod, confirmDialog, 1);
-
-        return new Object[] {label, yes, no, confirmDialog};
-    }
-
-    public static Object getButtonListener(ButtonAPI button) {
-        return RolfLectionUtil.invokeMethodDirectly(ClassRefs.buttonGetListenerMethod, button);
     }
 }
